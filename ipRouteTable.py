c7ce30db8608246da0901ecb9b40e7a130939eaa
dikdok6969 <43872463+dikdok6969@users.noreply.github.com>
2022-10-31 14:25:09 +0700

    update getnext

--- ipRouteTable.py
+++ ipRouteTable.py
@@ -1,23 +1,18 @@
-from pysnmp.entity.rfc3413.oneliner import cmdgen  
+from pysnmp.entity.rfc3413.oneliner import cmdgen
 
 errorIndication, errorStatus, errorIndex, \
-varBindTable = cmdgen.CommandGenerator().bulkCmd(  
-            cmdgen.CommunityData('test-agent', 'public'),  
-            cmdgen.UdpTransportTarget(('127.0.0.1', 161)),  
-            0, 
-            25, 
-            #(1,3,6,1,2,1,4,20), # ipAddrTable OID . This works fine.
-            (1,3,6,1,2,1,4,21) # ipRouteTable
-            #(1,3,6,1,2,1,4,22), # ipNetToMediaTable
-        )
+                 varBindTable = cmdgen.CommandGenerator().nextCmd(
+    cmdgen.CommunityData('test-agent', 'public'),
+    cmdgen.UdpTransportTarget(('localhost', 161)),
+    (1,3,6,1,2,1,4,21)
+    )
 
 if errorIndication:
-   print('1')
+    print('1')
 else:
     if errorStatus:
-        print ('2')
-            
+        print ("2")
     else:
         for varBindTableRow in varBindTable:
             for name, val in varBindTableRow:
-                print('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
+                print (f"{name.prettyPrint()} = {val.prettyPrint()}")
