859f5db2a40a6c26619dcb6c9a668bee9532d50f
shibahub <78590473+shibahub@users.noreply.github.com>
2022-10-31 22:33:07 +0700

    shiba ver

---
+++ system.py
@@ -0,0 +1,17 @@
+from pysnmp.entity.rfc3413.oneliner import cmdgen
+
+errorIndication, errorStatus, errorIndex, varBindTable = cmdgen.CommandGenerator().nextCmd(
+    cmdgen.CommunityData('test-agent', 'public'),
+    cmdgen.UdpTransportTarget(('localhost', 161)),
+    (1,3,6,1,2,1,1)
+    )
+
+if errorIndication:
+    print('1')
+else:
+    if errorStatus:
+        print ("2")
+    else:
+        for varBindTableRow in varBindTable:
+            for name, val in varBindTableRow:
+                print (f"{name.prettyPrint()} = {val.prettyPrint() or 'Unknown'}")
